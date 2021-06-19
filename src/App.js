4f73243d4e648b39e6059f2c23fbd5f77a83ff91
Ayush Chakraborty <ayushc@umd.edu>
2021-06-19 16:02:05 +0400

    bleh

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ import { Blog } from "./components/blog/Blog";
 import BlogPost from "./components/blog/BlogPost";
 import GetInTouch from "./components/home/GetInTouch.jsx";
 import Leadership from "./components/home/Leadership.jsx";
-import NewSkills from "./components/home/NewSkills";
 import Experience from "./components/home/Experience";
 
 const Home = React.forwardRef((props, ref) => {
